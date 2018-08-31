c4c30e97bbe43dedea0d78e465f628691bb7b73f
rene <renegmed@yahoo.com>
2018-08-31 14:51:26 -0400

    generate public key using private key

--- generate_wallet.go
+++ generate_wallet.go
@@ -7,6 +7,7 @@ package main
 *
  */
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"log"
 
@@ -34,5 +35,28 @@ func generate_wallet_main() {
 
 	// Note that private keys are used for signing transactions and is to be treated
 	// like a password
+
+	// Generating Public Key
+
+	// Since public key is derived from the private key, we can use crypto Public() method
+	// to generate public key.
+
+	publicKey := privateKey.Public()
+	// returns crypto.PublicKey
+	//type PublicKey struct {
+	//	elliptic.Curve
+	//	X, Y *big.Int
+	//}
+
+	// Convert to hex. Strip off the 0x and first 2 characters 04
+	// which is always the EC prefix and is not required
+	publicKeyECSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes))
+	fmt.Println(hexutil.Encode(publicKeyBytes)[4:])
 
 }
