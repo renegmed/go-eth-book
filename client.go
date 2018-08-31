d8b89fb8cef84c5c50cdf5fd1cb4e1ffd49da39e
rene <renegmed@yahoo.com>
2018-08-31 12:41:18 -0400

    Reading account balance, using nil to get the latest balance

--- client.go
+++ client.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
@@ -13,6 +15,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("we have a connection")
-	_ = client
+	// Reading the balance of account.
+
+	account := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+
+	// Setting nil as the block number will return the latest balance
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(balance) // 25943679348360745848
+
 }
