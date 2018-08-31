56716f0b2ffb793e24311b0b233deed39b8c6a55
rene <renegmed@yahoo.com>
2018-08-31 16:01:48 -0400

    Generate public address using keccak-256 hash of the public key

--- generate_wallet.go
+++ generate_wallet.go
@@ -8,12 +8,14 @@ package main
  */
 import (
 	"crypto/ecdsa"
+
 	"fmt"
 	"log"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/sha3"
 )
 
 func generate_wallet_main() {
@@ -50,13 +52,28 @@ func generate_wallet_main() {
 
 	// Convert to hex. Strip off the 0x and first 2 characters 04
 	// which is always the EC prefix and is not required
-	publicKeyECSA, ok := publicKey.(*ecdsa.PublicKey)
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
 	if !ok {
 		log.Fatal("error casting public key to ECDSA")
 	}
 
-	publicKeyBytes := crypto.FromECDSAPub(publicKeyECSA)
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
 	fmt.Println(hexutil.Encode(publicKeyBytes))
 	fmt.Println(hexutil.Encode(publicKeyBytes)[4:])
+
+	// Generate public address.
+	// crypto.PubkeyToAddress method accepts an ECDSA public key
+	// and returns public address
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+
+	// The public address is simply the Keccak-256 hash of the public key.
+	// We take the last 40 characters (20 bytes) and prefix it with 0x.
+
+	hash := sha3.NewKeccak256()
+	hash.Write(publicKeyBytes[1:])
+	fmt.Println(hexutil.Encode(hash.Sum(nil)))
+	fmt.Println(hexutil.Encode(hash.Sum(nil)[12:]))
 
 }
